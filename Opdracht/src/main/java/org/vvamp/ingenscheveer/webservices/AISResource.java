5abf3f551216816c41ed27b7d186091f7af071e1
Vincent van Setten <vvampus@gmail.com>
2024-06-12 18:45:13 +0200

    Clean APIs

--- Opdracht/src/main/java/org/vvamp/ingenscheveer/webservices/AISResource.java
+++ Opdracht/src/main/java/org/vvamp/ingenscheveer/webservices/AISResource.java
@@ -1,93 +1,34 @@
 package org.vvamp.ingenscheveer.webservices;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.vvamp.ingenscheveer.Main;
 import org.vvamp.ingenscheveer.models.json.AisSignal;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Path("/ais")
 public class AISResource {
     @GET
-    @Produces("application/json")
-    public Response getShip() {
-        ArrayList<AisSignal> shipMessages = Main.storageController.load();
-
-        if(shipMessages.size() == 0){
-            return Response.status(200).entity("").build();
-        }
-        AisSignal mostRecentUpdate = shipMessages.get(shipMessages.size()-1);
-        ObjectMapper mapper = new ObjectMapper();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUpdates(@QueryParam("limit") @DefaultValue("-1") int limit) {
+        List<AisSignal> shipMessages;
         try {
-            String json = mapper.writeValueAsString(mostRecentUpdate);
-            return Response.status(200).entity(json).build();
-        }catch(Exception e){
-            Map<String,String> messages = new HashMap<>();
-            messages.put("Error", "Failed to generate response data");
-            return Response.status(500).entity(messages).build();
-
-        }
-    }
-
-    @GET
-    @Path("/updates")
-    @Produces("application/json")
-    public Response getUpdates() {
-        JsonArrayBuilder jab = Json.createArrayBuilder();
-        ArrayList<AisSignal> shipMessages = Main.storageController.load();
-
-        if(shipMessages.size() == 0){
-            return Response.status(200).entity("").build();
-        }
-
-        ObjectMapper mapper = new ObjectMapper();
-
-            try {
-                String json = mapper.writeValueAsString(shipMessages);
-                return Response.status(200).entity(json).build();
-
-            } catch (JsonProcessingException e) {
-                Map<String,String> messages = new HashMap<>();
-                messages.put("Error", "Failed to generate response data");
-                return Response.status(500).entity(messages).build();
+            shipMessages = Main.storageController.load();
+            Collections.reverse(shipMessages);
+            if (limit > -1) {
+                shipMessages = shipMessages.stream().limit(limit).collect(Collectors.toList());
             }
+
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Map.of("Error", "Failed to generate response data")).build();
+        }
+        return Response.status(Response.Status.OK).entity(shipMessages).build();
     }
 
-    @GET
-    @Path("/updates/{count}")
-    @Produces("application/json")
-    public Response getUpdatesWithCount(@PathParam("count") int count) {
-        JsonArrayBuilder jab = Json.createArrayBuilder();
-        ArrayList<AisSignal> shipMessages = Main.storageController.load();
-
-        if(shipMessages.size() == 0){
-            return Response.status(200).entity("").build();
-        }
-
-        int actualCount = count <= shipMessages.size() ? count : shipMessages.size();
-        Collections.reverse(shipMessages); // from new to old
-        List<AisSignal> truncatedMessages = shipMessages.stream().limit(actualCount).collect(Collectors.toList()); // limit to 'count' newest
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            String json = mapper.writeValueAsString(truncatedMessages);
-            return Response.status(200).entity(json).build();
-
-        } catch (JsonProcessingException e) {
-            Map<String,String> messages = new HashMap<>();
-            messages.put("Error", "Failed to generate response data");
-            return Response.status(500).entity(messages).build();
-        }
-    }
 }
--- Opdracht/src/main/java/org/vvamp/ingenscheveer/webservices/FerryCrossingResource.java
+++ Opdracht/src/main/java/org/vvamp/ingenscheveer/webservices/FerryCrossingResource.java
@@ -7,7 +7,7 @@ import org.vvamp.ingenscheveer.models.FerryCrossing;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,14 +19,15 @@ public class FerryCrossingResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getCrossings(@QueryParam("limit") @DefaultValue("-1") int limit) {
 
-        List<FerryCrossing> crossings = new ArrayList<>();
+        List<FerryCrossing> crossings;
         try {
             crossings = Ferry.getFerry().getFerryCrossings();
+            Collections.reverse(crossings);
             if (limit > -1) {
                 crossings = crossings.stream().limit(limit).collect(Collectors.toList());
             }
         } catch (Exception e) {
-            return Response.status(500).entity(Map.of("Error", "Failed to retrieve FerryCrossing data")).build();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Map.of("Error", "Failed to retrieve FerryCrossing data")).build();
         }
 
         return Response.status(Response.Status.OK).entity(crossings).build();
